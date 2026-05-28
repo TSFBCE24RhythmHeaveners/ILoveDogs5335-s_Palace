46c6a4f26fafa447f30063cf88c24997616a184b
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-28 15:38:16 +0400

    Add an Even Or Odd checker.

---
+++ python/evenorodd.py
@@ -0,0 +1,7 @@
+#Start code here
+import math
+number = int(round(input("Enter a number.")))
+if number % 2 == 0:
+  print("The number ", number, " is even.")
+if number % 2 == 1:
+  print("The number ", number, " is odd.")
